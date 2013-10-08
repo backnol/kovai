ae0f3bdb808a05bb9f99ae9fd10a19eccf3652dd
Backnol Yogendran <backnol@gmail.com>
2013-10-08 20:22:32 +0000

    Visual changes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = Tweet.order('posted_at DESC').first(3)
+    @tweets = Tweet.order('posted_at DESC').first(4)
     render json: @tweets
   end
 end
